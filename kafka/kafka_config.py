9c19c8e5fd62be9dbaa96c74685563845907945d
amitpatel <amit.patel@vayana.com>
2024-06-02 23:46:45 +0530

    code refractoring

---
+++ kafka/kafka_config.py
@@ -0,0 +1,17 @@
+config = {
+    "API_KEY": "RVNSNVDJ6K5EMXAP",
+    "API_SECRET": "snIRE463nJhKOcrEnNv0bOjfwfRcohZbVeGyerJnSQ/O3/v/7+Tx4N7hyO6DeMpX",
+    "BOOTSTRAP_SERVER": "pkc-12576z.us-west2.gcp.confluent.cloud:9092",
+
+    'SCHEMA_REGISTRY_KEY': 'LXFV5IHALMTVNOQR',
+    'BASIC_AUTH_USER_INFO': 'hSlMzKwXdX1Fov7odLKeQb5Vjw/hpSwxjtUhAJpdJ947Z+G4nJCQaVfMnYaRflC/',
+    "SCHEMA_REGISTRY_URL": 'https://psrc-4nyjd.us-central1.gcp.confluent.cloud',
+    'BASIC_AUTH_CREDENTIAL_SOURCE': 'USER_INFO',
+
+    "KAFKA_ENDPOINT": "https://psrc-4nyjd.us-central1.gcp.confluent.cloud",
+    "AUTO_OFFSET_RESET": "Latest",
+    "GROUP_ID": "group12",
+    'security.protocol': 'SASL_SSL',
+    "sasl.mechanisms": 'PLAIN',
+
+}
